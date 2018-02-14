316aa12ac4ce0f7e945fafd7ed1cbd5b22e61f4e
Hung Nguyen <nguyenht33@gmail.com>
2018-02-13 23:33:46 -0500

    openweathermap functional

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-const geoKeys = 'AIzaSyBi4p4_5i-BkoRSKAhOIzUyp9usQTqQitw';
-const geoURL = 'https://maps.googleapis.com/maps/api/geocode/json'
-let userLocation;
+const geoKey = 'AIzaSyBi4p4_5i-BkoRSKAhOIzUyp9usQTqQitw';
+const geoURL = 'https://maps.googleapis.com/maps/api/geocode/json';
+const openKey = '335185afd1bee6c30739e6238eec798b';
+const openURL = 'https://api.openweathermap.org/data/2.5/weather';
 
 function getLocation(address) {
-  let position = {}
   let data = {
     address: address,
     sensor: false,
-    key: geoKeys
+    key: geoKey
   };
 
   $.ajax({
@@ -16,21 +16,43 @@ function getLocation(address) {
     url: geoURL,
     data: data,
     async: false,
-    success: displayLocation           
+    success: getCoordinates           
   });
 }
 
-function displayLocation(location) {
-  console.log(location);
-  $('.js-location-result').html(`<p>Latitude:${location.results[0].geometry.location.lat}</p><p>Longtitude:${location.results[0].geometry.location.lng}</p>`)
+function getCurrentWeather(lat, lon) {
+  let data = {
+    lat: lat,
+    lon: lon,
+    appid: openKey
+  };
+
+  $.ajax({
+    type: 'GET',
+    dataType: 'json',
+    url: openURL,
+    data: data,
+    async: false,
+    success: displayWeather
+  });
+}
+
+function getCoordinates(location) {
+  const lat = location.results[0].geometry.location.lat;
+  const lon = location.results[0].geometry.location.lng;
+  getCurrentWeather(lat, lon);
+}
+
+function displayWeather(weather) {
+  console.log(weather);
 }
 
 function getLocationInput() {
   $('.js-location-form').on('submit', function(e) {
     e.preventDefault();
     const locationTarget = $(event.currentTarget).find('.js-location-input');
-    userLocation = locationTarget.val();
-    getLocation(userLocation);
+    const address = locationTarget.val();
+    getLocation(address);
   })
 }
 
